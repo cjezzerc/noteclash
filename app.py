f96f3e1bca4961937888c423f75910e0a2dbcac1
Jeremy Craven <cjeremycraven@gmail.com>
2022-05-19 14:49:22 +0100

    various reorganisations of plot code

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template
-# import plotly.utils
-# import plotly.graph_objects as go
-# import pprint
+
 import time
+
+from flask import Flask, render_template
+
 import figure1
 
 app=Flask(__name__)
@@ -10,12 +10,8 @@ app=Flask(__name__)
 @app.route('/')
 def testfunc():
 
-    
-    # fig = go.Figure(data=go.Scatter(x=[1,2,3], y=[1,4,9], mode='markers'))
-    # pprint.pprint(fig)
-    # fig_json=json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    
-    fig_json=figure1.make_fig()
+    fig, fig_json=figure1.make_fig()
 
-    return render_template('first_template.html', time=time.time(), graphJSON=fig_json)
-
+    return render_template('first_template.html', 
+                            time=time.time(), 
+                            graphJSON=fig_json)
--- figure1.py
+++ figure1.py
@@ -1,17 +1,30 @@
 
-import plotly.utils
-import plotly.graph_objects as go
+
 import pprint
 import json
+import sys
+
+import plotly.utils
+import plotly.graph_objects as go
 
 def make_fig():
 
-    fig = go.Figure(data=go.Scatter(x=[1,2,3], y=[1,4,9], mode='markers'))
+    trace1 = go.Scatter(x=[256,256,440,440], y=[256,440,256,440], mode='markers')
+    traces=[trace1]
+
+    xaxis = go.layout.XAxis(range=[128,512])
+    yaxis = go.layout.YAxis(range=[128,512])
+
+    layout= go.Layout(width=500, height=500, xaxis=xaxis, yaxis=yaxis)
+
+    fig = go.Figure(data=traces, layout=layout)
     pprint.pprint(fig)
     fig_json=json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     
-    return fig_json
+    return fig, fig_json
 
 if __name__=="__main__":
     output_html_file=sys.argv[1]
-    make_fig().write_html(output_html_file)
+    fig, fig_json=make_fig()
+    fig.write_html(output_html_file)
+    
